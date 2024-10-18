9c29697149ebd87b4c32b75ba7010540511f3aa4
vasanthan <vasanthakumar036@gmail.com>
2024-10-18 16:50:19 +0530

    breeze installed migrations Modal

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes() : HasMany {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments() : HasMany {
+        return $this->hasMany(Comment::class);
+    }
+
+}
